511de77a84131900f8094e33905c2e5dc2846f40
smlund <lund@frib.msu.edu>
2016-02-09 14:28:27 -0500

    SML: 1st pass on generalizing species notation in scripts to simplify setup for other species

--- frib-front-xy.py
+++ frib-front-xy.py
@@ -24,8 +24,8 @@ This setup scripts for these simulations are large and have been broken
 apart into functional sections to aid maintainability.
  ** To be implemented **   
 
-    frib-front-xy.py        :  Main setup/control for xy slice simulations 
-    frib-front-xy-parms.py  :  Parameters for runs 
+    frib-front-xy.py             :  Main setup/control for xy slice simulations 
+    frib-front-xy-parms-name.py  :  Parameters for runs, name = ion type, U etc
 
     frib-front-lat.py       :  Lattice setup/description (3D, xy etc) 
     frib-front-lat-diag.py  :  Diagnostics for lattice     
@@ -56,41 +56,10 @@ setup()
 # Set runmaker - included in informational labels on output plots
 top.runmaker = "Wong and Lund"
 
-# Beam parameters for simulation
-#
-#   Other than numerical parameters, the physical simulation is specified
-#   by the numbers input immediately below.  
+# Read in parameters describing beam to be simulated
+#   - Use appropriate name input file for type of beam to be simulated. 
+execfile("frib-front-xy-params-U.py") 
 
-
-# --- Define species
-# 
-#     Syntax of later use:
-#
-#       for us in U_species:
-#         us.ppzx()
-#
-#     Dictionaries also setup. 
-
-# --- Define charge states 
-U_charge_states = [33,34,25,26,27,28,29,30,31,32,35,36,37,38,39,40]
-O_charge_states = [1,2,3,4]
-
-# --- Create species consistent with charge states 
-U_species = [Species(type=Uranium,charge_state=i,name='U%d'%i) for i in U_charge_states]
-O_species = [Species(type=Oxygen, charge_state=i,name='O%d'%i) for i in O_charge_states]
-
-# --- Count species 
-U_ns = len(U_species) 
-O_ns = len(O_species) 
-
-# --- Make abbreviated dictionary of species "sp" for cleaner use in later diagnostics 
-#       sp.keys()
-sp_U = {U_species[i].name:U_species[i] for i in range(U_ns)}
-sp_O = {O_species[i].name:O_species[i] for i in range(O_ns)}
-
-sp = {}
-sp.update(sp_U)
-sp.update(sp_O)
 
 # --- Setup for variable particle weights in slice code. 
 #     Notes: 
@@ -116,37 +85,6 @@ sp.update(sp_O)
 top.wpid = nextpid()       # pid index for variable weights: initialize on generate  
 uzp0pid  = nextpid() - 1   # pid index for initial uz to scale weights: initialize on generate  
 
-# --- Assign species colors to help distinguish on plots: 
-#        target U species: magenta/green
-#        O        species: blue 
-#        High   U species: cyan 
-#        Low    U species: red 
-sp['O1'].color = "blue"
-sp['O2'].color = "blue"
-sp['O3'].color = "blue"
-sp['O4'].color = "blue"
-
-sp['U25'].color = "red"
-sp['U26'].color = "red"
-sp['U27'].color = "red"
-sp['U28'].color = "red"
-sp['U29'].color = "red"
-sp['U30'].color = "red"
-sp['U31'].color = "red"
-sp['U32'].color = "red"
-
-sp['U33'].color = "magenta"
-sp['U34'].color = "green"
-
-sp['U35'].color = "cyan"
-sp['U36'].color = "cyan"
-sp['U37'].color = "cyan"
-sp['U38'].color = "cyan"
-sp['U39'].color = "cyan"
-sp['U40'].color = "cyan"
-
-# --- Target species tuple for later use 
-sp_target = ['U33','U34']
 
 # --- Reference species as average of target 
 A_ref = 0. 
@@ -161,144 +99,6 @@ Q_ref = Q_ref/len(sp_target)
 m_ref = A_ref*amu 
 q_ref = Q_ref*echarge 
 
-# --- Set species properties for injection 
-#        The beam kinetic energy (ekin) and axial velocity (vbeam) should not both
-#        be set unless done so consistently.  If one is zero the code sets from
-#        the other on generation:
-#           vbeam = 0    => set vbeam from ekin
-#           ekin  = 0    => set ekin  from vbeam
-#
-
-# --- --- unneutralized electric currents ... array elements corresponding to charge state arrays  
-U_ibeam = array([0.210,0.205,0.035,0.051,0.068,0.088,0.115,0.150,0.175,0.192,0.178,0.142,0.110,0.072,0.043,0.031])*mA 
-O_ibeam = array([0.300,0.300,0.300,0.200])*mA 
-
-# --- --- kinetic energy 
-SourceBias = 35.*keV  # source voltage: set for Q_ref*SourceBias/A =>  4.9264706 keV/u for U 
-
-U_ekin = array(U_charge_states)*SourceBias
-O_ekin = array(O_charge_states)*SourceBias 
-
-# --- --- initial ion thermal temp/phase-space area 
-# 
-#  Approach: Allow setting of thermal phase-space area by either a temperature specification 
-#            OR a normalized rms emittance measure. Convert input values to a normalized rms 
-#            edge emittance to work with the Warp loader.  
-#
-#  init_emit_spec = "emitn"    => set by init_emitn 
-#                   or "temp"  => set by init_temp 
-# 
-#  init_emitn = Initial ion thermal rms normalized emittance [m-rad] ** NOT edge measure **
-#  init_temp  = Initial ion temp [eV] 
-#
-#  Notes:
-#  * Guilliaume:  Ions likely 2-3 eV and electrons few to 100s of keV.  
-#                 Ions not equilibrated with electrons.
-#  * Previous simulations used 0.4*mm*mr thermal phase-space area launched outside of magnetic fields.      
-#  * Here we set only the thermal component of the emittance and the distribution is loaded without 
-#    correlation terms. Later we adjust correlation terms to set the value of the canonical angular 
-#    momentum as desired based on field values at launch and birth.    
-
-init_emit_spec = "temp" 
-init_temp  = 3.          # initial temp [eV] 
-init_emitn = 0.4*mm*mr   # initial rms normalized emittance [m-rad]  
-
-# --- --- initial beam size: elliptical cross-section beam 
-#   
-#   r_x  = 2*sqrt(<x^2>)           Initial x-plane beam rms edge radius [m] 
-#   rp_x = 2*<x*x'>/sqrt(<x^2>)    Initial rms envelope angle   [rad] 
-# 
-#   r_y, rp_y                      Analogous y-plane measures 
-#
-# Note:
-# * Now set r_x etc by fraction of ECR aperture size.  
-# * Previous simulations used betatron functions with a specific emittance value to get 
-#   desired beam size. 
-# * Was set by betatron functions and an emittance specification.  Commented out code (left 
-#   in for reference).   
-#
-
-# ####################### OLD ... set by betatron funcs ###############
-#alpha_x = 0.
-#beta_x  = 12.9*cm
-#gamma_x = (1. + alpha_x**2)/beta_x 
-#
-#alpha_y = 0.
-#beta_y  = 12.9*cm
-#gamma_y = (1. + alpha_y**2)/beta_y
-#
-#emitn_edge = 0.4*mm*mr   # norm rms edge emittance used to set beam size in earlier simulations 
-#
-#v_z_ref   = sqrt(2.*jperev*Q_ref*SourceBias/m_ref)  # nonrel approx ref z-velocity 
-#gamma_ref = 1./sqrt(1.-(v_z_ref/clight)**2)         # ref axial gamma factor (approx 1) 
-#emit_edge = emitn_edge/(gamma_ref*v_z_ref/clight)   # unnormalized rms edge emittance 
-#
-#r_x  = sqrt(emit_edge*beta_x)             # envelope x-edge 
-#r_y  = sqrt(emit_edge*beta_y)             # envelope y-edge 
-#rp_x = -sqrt(emit_edge/beta_x)*alpha_x    # envelope x-angle 
-#rp_y = -sqrt(emit_edge/beta_y)*alpha_y    # envelope y-angle 
-# #######################
-
-r_extractor = 4.*mm   # ECR extraction aperture radius [m]
-                      #   Value both for Venus and Artemis A,B ECR Sources 
-
-r_x = r_extractor  
-r_y = r_extractor
-rp_x = 0. 
-rp_y = 0. 
-
-
-## --- transverse thermal velocity and energy (eV) of nonrel ref particle from emittance 
-#
-#vt = v_z_ref*emit_edge/(2.*r_x) 
-#Et = 0.5*m_ref*vt**2/jperev 
-#
-## --- intrinsic thermal emittance scale 
-#Et_therm = 3.   # Guilliaume's estimated ion temp scale (eV) 
-#vt_therm = sqrt(2.*(jperev*Et_therm)/m_ref)
-#emit_therm  = 2.*r_x*vt_therm/v_z_ref
-#emitn_therm = (gamma_ref*v_z_ref/clight)*emit_therm
-#
-## Ratio of thermal to edge emittance suggests value of P_theta contributing to effective emittance 
-## emit_therm/emit_edge = 0.10  => most beam PS area from P_theta  
-
-# --- Set properties of initial species load 
-
-for i in range(U_ns):
-  Usp = U_species[i]
-  ekin_i  = U_ekin[i]
-  betab_i = sqrt(2.*jperev*ekin_i/Usp.sm)/clight
-  if init_emit_spec == "emitn":
-    emitn_therm_edge = 4.*init_emitn 
-  elif init_emit_spec == "temp": 
-    emitn_therm_edge = betab_i*((2.*r_x)/sqrt(2.))*sqrt(init_temp/ekin_i)
-  else:
-    raise Exception("Error: init_emit_spec not set properly") 
-  #
-  Usp.ekin   = ekin_i              # kinetic energy of beam particle [eV]
-  Usp.vbeam  = 0.                  # beam axial velocity [m/sec] (set from ekin if 0) 
-  Usp.ibeam  = U_ibeam[i]          # beam current [amps] 
-  Usp.emitnx = emitn_therm_edge    # beam x-emittance, rms edge [m-rad] 
-  Usp.emitny = emitn_therm_edge    # beam y-emittance, rms edge [m-rad]
-  Usp.vthz   = 0.                  # axial velocity spread [m/sec] 
-
-for i in range(O_ns):
-  Osp = O_species[i]
-  ekin_i  = O_ekin[i]
-  betab_i = sqrt(2.*jperev*ekin_i/Osp.sm)/clight
-  if init_emit_spec == "emitn":
-    emitn_therm_edge = 4.*init_emitn 
-  elif init_emit_spec == "temp": 
-    emitn_therm_edge = betab_i*((2.*r_x)/sqrt(2.))*sqrt(init_temp/ekin_i)
-  else:
-    raise Exception("Error: init_emit_spec not set properly") 
-  # 
-  Osp.ekin   = ekin_i  
-  Osp.vbeam  = 0.
-  Osp.ibeam  = O_ibeam[i]
-  Osp.emitnx = emitn_therm_edge 
-  Osp.emitny = emitn_therm_edge
-  Osp.vthz   = 0.
 
 # Calculate vbeam and other species quantities for defined species 
 derivqty()
@@ -321,47 +121,6 @@ for ii in sort(sp.keys()):
   brho  = gamma*s.mass*s.vbeam/s.charge   # Define rigidity with mean axial velocity 
   sp_brho.update({ii:brho})
   print("   Species: "+ii+" [B rho] = %s T-m"%brho)
-
-
-#
-# Beam centroid and rms envelope initial conditions at s=0      
-#    
-#   x0:   initial x-centroid xc = <x> [m]
-#   y0:   initial y-centroid yc = <y> [m]
-#   xp0:  initial x-centroid angle xc' = <x'> = d<x>/ds [rad]
-#   yp0:  initial y-centroid angle yc' = <y'> = d<y>/ds [rad]
-#
-#   a0:   initial x-envelope edge a = 2*sqrt(<(x-xc)^2>) [m]
-#   b0:   initial y-envelope edge b = 2*sqrt(<(y-yc)^2>) [m]
-#   ap0:  initial x-envelope angle ap = a' = d a/ds [rad]
-#   bp0:  initial y-envelope angle bp = b' = d b/ds [rad]
-
-for i in range(U_ns):
-  Usp = U_species[i]
-  # --- centroid 
-  Usp.x0  = 0.
-  Usp.y0  = 0.
-  Usp.xp0 = 0.
-  Usp.yp0 = 0.
-  # --- envelope 
-  Usp.a0   = r_x
-  Usp.b0   = r_y
-  Usp.ap0  = rp_x
-  Usp.bp0  = rp_y
-
-for i in range(O_ns):
-  Osp = O_species[i]
-  # --- centroid 
-  Osp.x0  = 0.   
-  Osp.y0  = 0.   
-  Osp.xp0 = 0.   
-  Osp.yp0 = 0.   
-  # --- envelope 
-  Osp.a0   = r_x              
-  Osp.b0   = r_y           
-  Osp.ap0  = rp_x  
-  Osp.bp0  = rp_y  
-
 
 #
 # Setup Lattice  
