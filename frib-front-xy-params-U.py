b9983a7d58b85c6f165eb702a18911df40cfa2cc
smlund <lund@frib.msu.edu>
2016-02-11 17:42:11 -0500

    SML: Cleaned up species setup to use dictionaries in the loading AND set default to include axial velocity spread (set to 3 eV for each ion)

--- frib-front-xy-params-U.py
+++ frib-front-xy-params-U.py
@@ -51,11 +51,17 @@ sp = {}
 sp.update(sp_Operate)
 sp.update(sp_Support)
 
+# --- Define "target" species list for later use in diagnostics 
+#       Target species are those desired to transport downstream. 
+sp_target = ['U33','U34']
+
 # --- Assign species colors to help distinguish on plots: 
-#        target U species: magenta/green
+#        
 #        O        species: blue 
 #        High   U species: cyan 
+#        Target U species: magenta/green 
 #        Low    U species: red 
+
 sp['O1'].color = "blue"
 sp['O2'].color = "blue"
 sp['O3'].color = "blue"
@@ -80,19 +86,10 @@ sp['U38'].color = "cyan"
 sp['U39'].color = "cyan"
 sp['U40'].color = "cyan"
 
-# --- Target species list for later use in diagnostics 
-sp_target = ['U33','U34']
+# --- Species unneutralized electric currents
+#      * Set in a species named dictionary ibeam. 
+#      * Data from ECR measurement data.    
 
-# --- Set species properties for injection 
-#        The beam kinetic energy (ekin) and axial velocity (vbeam) should not both
-#        be set unless done so consistently.  If one is zero the code sets from
-#        the other on generation:
-#           vbeam = 0    => set vbeam from ekin
-#           ekin  = 0    => set ekin  from vbeam
-#
-
-# --- --- unneutralized electric currents
-#           Set in a named dictionary ibeam. Data from ECR measurement data.    
 Operate_ibeam = array([0.210,0.205,0.035,0.051,0.068,0.088,0.115,0.150,0.175,0.192,0.178,0.142,0.110,0.072,0.043,0.031])*mA 
 Support_ibeam = array([0.300,0.300,0.300,0.200])*mA 
 
@@ -101,7 +98,7 @@ ibeam = {
 'U26': 0.051*mA,
 'U27': 0.068*mA,
 'U28': 0.088*mA,
-'U39': 0.115*mA,
+'U29': 0.115*mA,
 'U30': 0.150*mA,
 'U31': 0.175*mA,
 'U32': 0.192*mA,
@@ -119,76 +116,67 @@ ibeam = {
 'O4': 0.200*mA
          }
 
-# --- --- kinetic energy
-#          Set as Q*SourceBias in a dictionary ekin 
+# --- Species kinetic energy at injection
+#      * Set in a species named dictionary ekin  
+#      * Set as Q*SourceBias where Q is the species charge state  
+
+
 SourceBias = 35.*keV  # source voltage: set for Q_ref*SourceBias/A =>  4.9264706 keV/u for U 
 
+# *** remove following 2 lines of code when envelope solver modified to not use **
 Operate_ekin = array(Operate_charge_states)*SourceBias
 Support_ekin = array(Support_charge_states)*SourceBias 
 
 ekin = {key: SourceBias*charge_states[key] for key in sp.keys()} 
 
-# --- --- initial ion thermal phase-space area 
-#           Set in species-named dictionaries
-#            * Can set individual values as in ibeam. 
+# --- Species initial thermal phase-space area measures  
+#      * Set in species-named dictionaries:
+#          temp_p = transverse   temperature [eV]  (after extractor accel value)
+#          temp_z = longitudinal temperature [eV]  (after extractor accel value)
+#          emitnx = normalized x-plane thermal rms emittance ** NOT edge measure **
+#          emitny = normalized y-plane thermal rns emittance ** NOT edge measure **
+#      * Can set individual dictionary values as in ibeam to tune as necessary.   
 # 
-#  Approach: Allow setting of thermal phase-space area by either a temperature specification 
+#  Approach: Allow setting of transverse thermal phase-space area by either a 
+#            temperature specification 
 #            OR a normalized rms emittance measure. Convert input values to a normalized rms 
 #            edge emittance to work with the Warp loader.  
 #
-#  init_ps_spec = "emitn"    => set by init_emitn 
+#  init_ps_spec = "emitn"    => set by emitnx and emitny 
 #                 or "temp"  => set by init_temp 
 # 
-#  emitn = Initial ion thermal rms normalized emittance [m-rad] ** NOT edge measure **
-#  temp  = Initial ion temp [eV] 
-#
 #  Notes:
 #  * Guilliaume:  Ions likely 2-3 eV and electrons few to 100s of keV.  
 #                 Ions not equilibrated with electrons.
-#  * Previous simulations used 0.4*mm*mr thermal phase-space area launched outside of magnetic fields.      
-#  * Here we set only the thermal component of the emittance and the distribution is loaded without 
-#    correlation terms. Later we adjust correlation terms to set the value of the canonical angular 
-#    momentum as desired based on field values at launch and birth. 
-#  * Syntax is used to allow different beam sizes and temperatures for each species. Initial runs 
-#    may employ a common value for all species    
+#  * Previous simulations used 0.4*mm*mr thermal phase-space area 
+#    launched outside of magnetic field.      
+#  * We set only the *thermal* component of the emittance and the distribution without 
+#    correlation terms. Later, correlation terms are set to produce the value of canonical angular 
+#    momentum desired based on axial magnetic field values at launch or birth. 
+#  * Syntax is used to allow different phase space area measures for each species. Initial runs 
+#    may employ a common value for all species.    
 
 init_ps_spec = "temp" 
-temp_ion  = 3.          # initial ion temp [eV] 
-emitn     = 0.4*mm*mr   # initial rms normalized emittance [m-rad]  ** NOT edge measure **
 
-Operate_temp = temp_ion*ones(Operate_ns)  
-Support_temp = temp_ion*ones(Support_ns) 
 
-temp = {key: temp_ion for key in sp.keys()} 
+temp_perp_ion  = 3.          # initial transverse ion temp [eV] 
+temp_z_ion     = 3.          # initial z          ion temp [eV] 
+emitn          = 0.4*mm*mr   # initial rms normalized emittance [m-rad]  ** NOT edge measure **
 
-Operate_emitnx = emitn*ones(Operate_ns)
-Operate_emitny = emitn*ones(Operate_ns)
-
-Support_emitnx = emitn*ones(Support_ns)
-Support_emitny = emitn*ones(Support_ns)
+temp_p = {key: temp_perp_ion for key in sp.keys()} 
+temp_z = {key: temp_z_ion    for key in sp.keys()} 
 
 emitnx = {key: emitn for key in sp.keys()}
 emitny = {key: emitn for key in sp.keys()}
 
-# --- --- initial beam centroid 
+
+# --- Species initial beam centroid 
 # 
 #   xc  = <x>   Initial centroid coordinate [m]
 #   xcp = <x'>  Initial centroid angle [rad] 
 # 
-#               Analogous y-plane measures 
+#   + Analogous y-plane measures 
 #
-
-Operate_xc  = zeros(Operate_ns) 
-Operate_xcp = zeros(Operate_ns)
-
-Operate_yc  = zeros(Operate_ns) 
-Operate_ycp = zeros(Operate_ns)
-
-Support_xc  = zeros(Operate_ns) 
-Support_xcp = zeros(Operate_ns)
-
-Support_yc  = zeros(Operate_ns) 
-Support_ycp = zeros(Operate_ns)
 
 xc  = {key: 0. for key in sp.keys()}
 yc  = {key: 0. for key in sp.keys()}
@@ -197,15 +185,16 @@ xcp = {key: 0. for key in sp.keys()}
 ycp = {key: 0. for key in sp.keys()}
 
 
-# --- --- initial beam size: elliptical cross-section beam 
+# --- Species initial beam size: elliptical cross-section beam 
+#      * Set as species-named dictionaries with:
 #   
-#   r_x  = 2*sqrt(<x^2>)           Initial x-plane beam rms edge radius [m] 
-#   rp_x = 2*<x*x'>/sqrt(<x^2>)    Initial rms envelope angle   [rad] 
+#   rx  = 2*sqrt(<x^2>)           Initial x-plane beam rms edge radius [m] 
+#   rxp = 2*<x*x'>/sqrt(<x^2>)    Initial rms envelope angle   [rad] 
 # 
-#   r_y, rp_y                      Analogous y-plane measures 
+#   ry, ryp  are analogous y-plane measures 
 #
-# Note:
-# * Now set r_x etc by fraction of ECR aperture size.  
+# Notes:
+# * Now set rx etc by fraction of ECR aperture size.  
 # * Previous simulations used betatron functions with a specific emittance value to get 
 #   desired beam size. Code used was (y-plane similar):   
 #     alpha_x = 0.
@@ -218,23 +207,11 @@ ycp = {key: 0. for key in sp.keys()}
 #     gamma_ref = 1./sqrt(1.-(v_z_ref/clight)**2)         # ref axial gamma factor (approx 1) 
 #     emit_edge = emitn_edge/(gamma_ref*v_z_ref/clight)   # unnormalized rms edge emittance 
 #
-#     r_x  = sqrt(emit_edge*beta_x)             # envelope x-edge 
-#     rp_x = -sqrt(emit_edge/beta_x)*alpha_x    # envelope x-angle 
+#     rx  = sqrt(emit_edge*beta_x)             # envelope x-edge 
+#     rxp = -sqrt(emit_edge/beta_x)*alpha_x    # envelope x-angle 
 
 r_extractor = 4.*mm   # ECR extraction aperture radius [m]
                       #   Value both for Venus and Artemis A,B ECR Sources 
-
-Operate_r_x = r_extractor*ones(Operate_ns)  
-Operate_r_y = r_extractor*ones(Operate_ns)
-
-Operate_rp_x = 0.*ones(Operate_ns) 
-Operate_rp_y = 0.*ones(Operate_ns) 
-
-Support_r_x = r_extractor*ones(Operate_ns)  
-Support_r_y = r_extractor*ones(Operate_ns)
-
-Support_rp_x = 0.*ones(Operate_ns) 
-Support_rp_y = 0.*ones(Operate_ns) 
 
 rx  = {key: r_extractor for key in sp.keys()}
 ry  = {key: r_extractor for key in sp.keys()}
@@ -243,7 +220,8 @@ rxp = {key: 0. for key in sp.keys()}
 ryp = {key: 0. for key in sp.keys()}
 
 
-# Specify ion birth method to set beam canonical angular momentum. 
+# --- Species initial canonical angular momentum. 
+#
 # brith_mode = 0: Ions are born at launch point
 #              1: Ions are born with a canonical angular momentum specified by the user
 #              2: Ions are born in average field between the two peaks in B-field
@@ -256,8 +234,9 @@ ryp = {key: 0. for key in sp.keys()}
 birth_mode = 2
 
 ## Input for birth_mode == 1
-rms_birth = 2.82
-bz0_birth = 1.2
+rms_birth = 2.82        # x-rms sqrt(<x^2>) [m] 
+bz0_birth = 1.2         # magnetic field at birth point [T].
+                        #  1.2 T is average field between Venus ECR peaks   
 
 ## Reference: rms_launch = 2.8 mm, bz0_launch = 2.15 T
 
